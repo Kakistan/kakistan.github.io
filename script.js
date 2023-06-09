aaf74f82996cd3edfd6b8e4aae63d875d808038d
Kakistan <axscott2703@gmail.com>
2023-06-09 23:46:03 +0100

    Revert filter test

--- script.js
+++ script.js
@@ -132,10 +132,6 @@ var sources = [
     { name: 'Football Insider', type: 'media', tier: 4, link: 'footyinsider247' },
 ];
 
-var sortedSources = sources.sort(function(a, b) {
-    return a.tier - b.tier;
-  });
-  
 sources.forEach(source => {
     document.querySelector(`.tier-${source.tier} .tier-content`).appendChild(
         document.createRange().createContextualFragment(
@@ -152,40 +148,3 @@ document.querySelectorAll('.tier-content').forEach(node => {
         node.insertBefore(document.createRange().createContextualFragment('<div class="flex-break"></div>'), element);
     }
 })
-
-window.onload = function() {
-    // Trigger the displaySources function whenever the dropdown menu value changes
-    var specialtyFilter = document.getElementById('specialty-filter');
-    specialtyFilter.addEventListener('change', displaySources);
-};
-
-function displaySources() {
-    var selectedSpecialty = document.getElementById('specialty-filter').value;
-
-    // Clear the existing tier content
-    var tiers = document.getElementsByClassName('tier-content');
-    for (var i = 0; i < tiers.length; i++) {
-        tiers[i].innerHTML = '';
-    }
-
-    // Filter the sources based on the selected specialty
-    var filteredSources = sources;
-    if (selectedSpecialty !== '') {
-        filteredSources = sources.filter(function(source) {
-            return source.specialty.includes(selectedSpecialty);
-        });
-    }
-
-    // Display the filtered sources in their respective tiers
-    for (var tier = 1; tier <= 4; tier++) {
-        var tierContent = document.querySelector('.tier-' + tier + ' .tier-content');
-        var tierSources = filteredSources.filter(function(source) {
-            return source.tier === tier;
-        });
-
-        tierSources.forEach(function(source) {
-            var sourceElement = createSourceElement(source);
-            tierContent.appendChild(sourceElement);
-        });
-    }
-}
